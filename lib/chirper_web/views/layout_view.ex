c9c8711559d39fe6f486d7fa902077a01e69bd52
Santosh Suresh <santosh.deshpande@gmail.com>
2019-02-06 16:48:45 +0530

    Completed Chapter 05 - #1

--- lib/chirper_web/views/layout_view.ex
+++ lib/chirper_web/views/layout_view.ex
@@ -4,6 +4,7 @@ defmodule ChirperWeb.LayoutView do
   def title("about.html"), do: build_title("About")
   def title("home.html"), do: build_title("Home")
   def title("help.html"), do: build_title("Help")
+  def title("contact.html"), do: build_title("Contact")
 
   def title(_), do: "Chirper"
 
--- test/chirper_web/controllers/page_controller_test.exs
+++ test/chirper_web/controllers/page_controller_test.exs
@@ -2,20 +2,26 @@ defmodule ChirperWeb.PageControllerTest do
   use ChirperWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
+    conn = get(conn, Routes.page_path(conn, :home))
     assert html_response(conn, 200) =~ "Chirper"
     assert html_response(conn, 200) =~ "Home | Chirper"
   end
 
   test "GET /help", %{conn: conn} do
-    conn = get(conn, "/help")
+    conn = get(conn, Routes.page_path(conn, :help))
     assert html_response(conn, 200) =~ "Help"
     assert html_response(conn, 200) =~ "Help | Chirper"
   end
 
   test "GET /about", %{conn: conn} do
-    conn = get(conn, "/about")
+    conn = get(conn, Routes.page_path(conn, :about))
     assert html_response(conn, 200) =~ "About"
     assert html_response(conn, 200) =~ "About | Chirper"
+  end
+
+  test "GET /contact", %{conn: conn} do
+    conn = get(conn, Routes.page_path(conn, :contact))
+    assert html_response(conn, 200) =~ "Contact"
+    assert html_response(conn, 200) =~ "Contact | Chirper"
   end
 end
